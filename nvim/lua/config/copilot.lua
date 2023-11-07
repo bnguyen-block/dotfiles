a81bc2a0ed4f7d2cc0b3abf707a9c7325a09b026
bnguyen-block <bnguyen@squareup.com>
2023-11-07 14:17:16 +1100

    block: Auto refresh copilot suggestion

--- nvim/lua/config/copilot.lua
+++ nvim/lua/config/copilot.lua
@@ -1,7 +1,6 @@
 require("copilot").setup({
 	panel = {
 		enabled = true,
-<<<<<<< HEAD
 		auto_refresh = true,
 		keymap = {
 			jump_prev = "[[",
@@ -31,9 +30,6 @@ require("copilot").setup({
 	filetypes = {
 		yaml = true,
 		markdown = true,
-||||||| parent of 68e0634 (block: Add copilot)
-=======
-		auto_refresh = false,
 		keymap = {
 			jump_prev = "[[",
 			jump_next = "]]",
@@ -45,30 +41,6 @@ require("copilot").setup({
 			position = "bottom", -- | top | left | right
 			ratio = 0.4,
 		},
-	},
-	suggestion = {
-		enabled = true,
-		auto_trigger = true,
-		debounce = 75,
-		keymap = {
-			accept = "<C-space>",
-			accept_word = false,
-			accept_line = false,
-			next = "<C-right>",
-			prev = "<C-left>",
-			dismiss = "<C-]>",
-		},
-	},
-	filetypes = {
-		yaml = false,
-		markdown = false,
->>>>>>> 68e0634 (block: Add copilot)
-		help = false,
-		gitrebase = false,
-		hgcommit = false,
-		svn = false,
-		cvs = false,
-		["."] = false,
 	},
 	copilot_node_command = "node", -- Node.js version must be > 16.x
 	server_opts_overrides = {},
