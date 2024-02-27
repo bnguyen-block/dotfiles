62bfa2dab6ef27f4a5c68dda2ef47318922e05b3
bnguyen-block <bnguyen@squareup.com>
2024-02-27 16:05:45 +1100

    vim: Allow copilot in markdown and yaml

--- nvim/lua/config/copilot.lua
+++ nvim/lua/config/copilot.lua
@@ -30,17 +30,12 @@ require("copilot").setup({
 	filetypes = {
 		yaml = true,
 		markdown = true,
-		keymap = {
-			jump_prev = "[[",
-			jump_next = "]]",
-			accept = "<CR>",
-			refresh = "gr",
-			open = "<M-CR>",
-		},
-		layout = {
-			position = "bottom", -- | top | left | right
-			ratio = 0.4,
-		},
+		help = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
 	},
 	copilot_node_command = "node", -- Node.js version must be > 16.x
 	server_opts_overrides = {},
