16908f14dfe557cc2d755386d892b2d2bb0dec82
bangn <thebangnguyen@gmail.com>
2024-11-17 00:50:32 +1100

    nvim: Use lazyvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+<<<<<<< HEAD
 
 -- COQ settings
 vim.g.coq_v1 = true
@@ -14,3 +15,6 @@ vim.g.coq_settings = {
   },
   auto_start = true,
 }
+||||||| parent of ebf9337 (nvim: Use lazyvim config)
+=======
+>>>>>>> ebf9337 (nvim: Use lazyvim config)
