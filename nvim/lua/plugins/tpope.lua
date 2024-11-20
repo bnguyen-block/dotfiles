acdc8fe5db99e2855aa1c4145b6d844ae63ef43c
bangn <thebangnguyen@gmail.com>
2024-11-20 14:39:24 +1100

    nvim: Use vim-surround instead of mini.surround

--- nvim/lua/plugins/tpope.lua
+++ nvim/lua/plugins/tpope.lua
@@ -1,30 +1,8 @@
 return {
-<<<<<<< HEAD
-<<<<<<< HEAD
   { "tpope/vim-abolish",    lazy = false },
   { "tpope/vim-endwise",    lazy = false },
   { "tpope/vim-eunuch",     lazy = false },
   { "tpope/vim-repeat",     lazy = false },
   { "tpope/vim-surround",   lazy = false },
-||||||| parent of ebf9337 (nvim: Use lazyvim config)
-=======
-  { "tpope/vim-abolish", lazy = false },
-  { "tpope/vim-endwise", lazy = false },
-  { "tpope/vim-eunuch", lazy = false },
-  { "tpope/vim-fugitive", lazy = false },
-  { "tpope/vim-repeat", lazy = false },
->>>>>>> ebf9337 (nvim: Use lazyvim config)
-||||||| parent of 47a5d2e (nvim: Update lazyvim config)
-  { "tpope/vim-abolish", lazy = false },
-  { "tpope/vim-endwise", lazy = false },
-  { "tpope/vim-eunuch", lazy = false },
-  { "tpope/vim-fugitive", lazy = false },
-  { "tpope/vim-repeat", lazy = false },
-=======
-  { "tpope/vim-abolish",    lazy = false },
-  { "tpope/vim-endwise",    lazy = false },
-  { "tpope/vim-eunuch",     lazy = false },
-  { "tpope/vim-repeat",     lazy = false },
->>>>>>> 47a5d2e (nvim: Update lazyvim config)
   { "tpope/vim-unimpaired", lazy = false },
 }
