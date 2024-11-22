b90258d8b3de9da723d23925134335995afd1025
Bang Nguyen <bnguyen@squareup.com>
2024-11-22 11:01:09 +1100

    Sync with upstream

--- home-manager/packages/commonPackages.nix
+++ home-manager/packages/commonPackages.nix
@@ -26,12 +26,8 @@ with pkgs; [
   lua
   luajitPackages.luacheck
   luarocks
-<<<<<<< HEAD
   markdownlint-cli
   markdownlint-cli2
-||||||| parent of 47a5d2e (nvim: Update lazyvim config)
-=======
->>>>>>> 47a5d2e (nvim: Update lazyvim config)
   mdl
   mise
   mkcert
