3d97a2acd02a2efc2c2431cbfc531a3e3274d963
Bang Nguyen <bnguyen@squareup.com>
2025-01-06 16:56:55 +1100

    block: Cleanup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-<<<<<<< HEAD
 
 -- COQ settings
 vim.g.coq_v1 = true
@@ -15,6 +14,3 @@ vim.g.coq_settings = {
   },
   auto_start = true,
 }
-||||||| parent of ebf9337 (nvim: Use lazyvim config)
-=======
->>>>>>> ebf9337 (nvim: Use lazyvim config)
