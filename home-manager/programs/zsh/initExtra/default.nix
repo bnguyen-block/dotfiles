f23c0b08465f06ffc7f902341b3eff37d9cc9903
Bang Nguyen <bnguyen@squareup.com>
2025-01-07 15:23:35 +1100

    block: Use correct shell order

--- home-manager/programs/zsh/initExtra/default.nix
+++ home-manager/programs/zsh/initExtra/default.nix
@@ -3,8 +3,8 @@ let
   functions = builtins.readFile ./functions.sh;
   git = builtins.readFile ./git.sh;
   keybindings = builtins.readFile ./keybindings.sh;
-  zshrc = builtins.readFile ./zshrc.sh;
-  prompt = builtins.readFile ./prompt.sh;
   mise = builtins.readFile ./mise.sh;
+  prompt = builtins.readFile ./prompt.sh;
+  zshrc = builtins.readFile ./zshrc.sh;
 in
-  block + functions + git + keybindings + prompt + zshrc + mise
+  block + functions + git + keybindings + zshrc + prompt + mise
