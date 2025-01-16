076f938f5f94ddeecbf67bfb227ded449ab246bb
Bang Nguyen <bnguyen@squareup.com>
2025-01-17 10:38:36 +1100

    block: Add package envs

--- home-manager/programs/zsh/initContent/block.sh
+++ home-manager/programs/zsh/initContent/block.sh
@@ -1,6 +1,10 @@
 # shellcheck disable=SC2164
 
 export BLOCK_DIR="$HOME/Development/"
+export PATH=$PATH:~/bin
+export NODE_EXTRA_CA_CERTS=${HOMEBREW_PREFIX:-/opt/homebrew}/etc/ca-certificates/cert.pem
+export COREPACK_NPM_REGISTRY=https://artifactory.global.square/artifactory/api/npm/square-npm/
+export COREPACK_INTEGRITY_KEYS=0
 
 alias gcurl=grpcurl
 
