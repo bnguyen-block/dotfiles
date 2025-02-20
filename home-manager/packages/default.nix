064806a58765614fde2244091840117f79583101
Bang Nguyen <bnguyen@squareup.com>
2025-02-21 10:46:42 +1100

    nix: Remove isLinux definition
    Use from stdenv.isLinux

--- home-manager/packages/default.nix
+++ home-manager/packages/default.nix
@@ -4,7 +4,6 @@
   ...
 }:
 with builtins; let
-  isLinux = !(isNull (match ".*linux.*" currentSystem));
   commonPackages = import ./commonPackages.nix {inherit pkgs pkgsUnstable;};
   macosPackages = import ./macosPackages.nix {inherit pkgs pkgsUnstable;};
   linuxPackages = import ./linuxPackages.nix {inherit pkgs pkgsUnstable;};
@@ -12,7 +11,7 @@ with builtins; let
 in
   commonPackages
   ++ (
-    if isLinux
+    if pkgs.stdenv.isLinux
     then linuxPackages
     else macosPackages
   )
