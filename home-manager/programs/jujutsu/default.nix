16c0eb78cb499248355b92128bf9cdcb1f71a1c3
Bang Nguyen <bnguyen@squareup.com>
2025-02-21 15:45:46 +1100

    jujutsu: Use delta as pager

--- home-manager/programs/jujutsu/default.nix
+++ home-manager/programs/jujutsu/default.nix
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 {
   pkgs,
   gitDetails,
@@ -12,7 +10,7 @@
       name = gitDetails.username;
       email = gitDetails.email;
     };
-    
+
     ui = {
       color = "auto"; # "always", "never", or "auto"
       default-command = "log"; # Default command if none is specified
@@ -21,25 +19,6 @@
         tool = "delta"; # Specify diff tool ("git", "delta", etc.)
         format = "git";
       };
-||||||| parent of f6fefad (nix: Add jj config and use jj from home-manager)
-=======
-{
-  pkgs,
-  gitDetails,
-  ...
-}: {
-  enable = true;
-
-  settings = {
-    user = {
-      name = gitDetails.username;
-      email = gitDetails.email;
-    };
-    ui = {
-      color = "auto"; # "always", "never", or "auto"
-      default-command = "log"; # Default command if none is specified
-      diff.tool = "delta"; # Specify diff tool ("git", "delta", etc.)
->>>>>>> f6fefad (nix: Add jj config and use jj from home-manager)
     };
   };
 }
