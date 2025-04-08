f906e29cf46fdb2f23767e68903c9fe82cdabc89
Bang Nguyen <bnguyen@squareup.com>
2025-04-08 15:00:39 +1000

    nvim: I actually like coq than blink

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -3,5 +3,6 @@ return {
   { "coq_nvim", enabled = true },
   { "friendly-snippets", enabled = false },
   { "nvim-autopairs", enabled = false },
+  { "nvim-cmp", enabled = false },
   { "render-markdown.nvim", enabled = false },
 }
