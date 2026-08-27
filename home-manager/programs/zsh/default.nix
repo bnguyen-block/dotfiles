26cdc678577da23427c50914dc975e38d620046e
Bang Nguyen <bnguyen@squareup.com>
2026-08-27 14:03:41 +1000

    zsh: Remove redundant settings

--- home-manager/programs/zsh/default.nix
+++ home-manager/programs/zsh/default.nix
@@ -40,15 +40,6 @@
       };
     }
     {
-      name = "zsh-autosuggestions";
-      src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-autosuggestions";
-        rev = "v0.6.4";
-        sha256 = "0h52p2waggzfshvy1wvhj4hf06fmzd44bv6j18k3l9rcx6aixzn6";
-      };
-    }
-    {
       name = "zsh-completions";
       src = pkgs.fetchFromGitHub {
         owner = "zsh-users";
